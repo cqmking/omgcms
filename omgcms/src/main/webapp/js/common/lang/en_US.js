7524eec34c9d0bfd6c29f39f5188517ebf040ceb
madfrogyang <madfrogyang@163.com>
2017-09-16 17:30:40 +0800

    新增前端国际化支持

---
+++ omgcms/src/main/webapp/js/common/lang/en_US.js
@@ -0,0 +1,12 @@
+function getMessage() {
+
+	const _message = {
+		en_US : {
+			message : {
+				hello : 'Hello World！'
+			}
+		}
+	};
+
+	return _message;
+}
---
+++ omgcms/src/main/webapp/js/common/lang/zh_CN.js
@@ -0,0 +1,11 @@
+function getDefaultMessage() {
+
+	const _defaultMessage = {
+		message : {
+			lang : 'zh_CN',
+			hello : '测试Hello World！'
+		}
+	};
+
+	return _defaultMessage;
+}
